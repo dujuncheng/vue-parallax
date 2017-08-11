fac67886ad790baf734179bf53b63db57bb5cbec
dujuncheng <1390851423@qq.com>
2017-08-11 14:08:05 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,49 @@
+<template>
+  <div id="app">
+    <imgcontainer :bgimg="'1.jpg'" :title="'标题一'" :index="'0'" :leaveTitleIndex="leaveTitleIndex" :leaveTitleTop="leaveTitleTop" :scrollY="scrollY"></imgcontainer>
+    <imgcontainer :bgimg="'2.jpg'" :title="'标题二'" :index="'1'" :leaveTitleIndex="leaveTitleIndex" :leaveTitleTop="leaveTitleTop" :scrollY="scrollY"></imgcontainer>
+    <imgcontainer :bgimg="'3.jpg'" :title="'标题三'" :index="'2'" :leaveTitleIndex="leaveTitleIndex" :leaveTitleTop="leaveTitleTop" :scrollY="scrollY"></imgcontainer>
+    <imgcontainer :bgimg="'4.jpg'" :title="'标题四'" :index="'3'" :leaveTitleIndex="leaveTitleIndex" :leaveTitleTop="leaveTitleTop" :scrollY="scrollY"></imgcontainer>
+  </div>
+</template>
+
+<script>
+import imgcontainer from './components/section'
+
+export default {
+  data () {
+    return {
+      imgArr: [1, 2, 3, 4],
+      chooseOne: '',
+//     第几个元素的title的index
+      leaveTitleIndex: '',
+//      第几个元素title的顶部到浏览器顶部的距离
+      leaveTitleTop: '',
+      scrollY: ''
+    }
+  },
+  name: 'app',
+  components: {
+    imgcontainer
+  },
+  mounted () {
+    var self = this
+    var containerArr = document.getElementsByClassName('container')
+    window.addEventListener('scroll', function () {
+      self.scrollY = window.scrollY
+      for (var i = 0; i < containerArr.length; i++) {
+        var itemtitle = document.querySelector('.title' + i)
+        var titleTop = itemtitle.offsetTop - window.scrollY
+        if (titleTop > -window.innerHeight / 2 && titleTop < window.innerHeight / 2) {
+          self.leaveTitleTop = titleTop
+          self.leaveTitleIndex = i
+        }
+      }
+    })
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/components/section.vue
@@ -0,0 +1,56 @@
+<template>
+  <div class="container" :class="'container' + index ">
+    <div class="bgimg" :style="imgStyle">
+      <div class="title" :class="'title' + index" :style="titleLeave">
+        <h1>{{title}}</h1>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script type="text/ecmascript-6">
+  export default {
+    props: ['bgimg', 'title', 'index', 'leaveTitleIndex', 'leaveTitleTop', 'scrollY'],
+    data () {
+      return {
+        container: []
+      }
+    },
+    computed: {
+      titleLeave () {
+        if (this.index + '' === this.leaveTitleIndex + '') {
+          return { opacity: this.leaveTitleTop / (window.innerHeight / 2), transform: 'translateY(' + (-10000) / this.leaveTitleTop + 'px)' }
+        } else {
+          return { opacity: '1' }
+        }
+      },
+      imgStyle () {
+        return { backgroundPosition: '50% ' + this.scrollY * 100 / document.body.offsetHeight + '%', backgroundImage: 'url( /static/img/' + this.bgimg + ')', backgroundSize: 'auto 140vh' }
+      }
+    }
+  }
+</script>
+
+<style scoped>
+.container{
+  height: 100vh;
+}
+.bgimg{
+  height: 100vh;
+  background-repeat: no-repeat;
+  background-attachment: fixed;
+  background-size: cover;
+  display: flex;
+  align-items: center;
+  justify-content: flex-end;
+  padding: 0px 40px 0px 40px;
+}
+  .title{
+    font-size: 30px;
+    color: ghostwhite;
+    background-color: rgba(0, 0, 0, 0.5);
+    width: 40%;
+    text-align: right;
+    padding:0px 40px;
+  }
+</style>
